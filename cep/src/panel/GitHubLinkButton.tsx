559b9898fa8ecaaebbcb89aff121e226144bdf6c
CasseShimada <c382304209@gmail.com>
2026-07-04 07:01:22 +0800

    Extract GitHub link button

---
+++ cep/src/panel/GitHubLinkButton.tsx
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import { ActionButton, Tooltip, TooltipTrigger } from '@adobe/react-spectrum';
+import { GITHUB_REPOSITORY_URL, openExternalUrl } from './externalLinks';
+import GitHubIcon from './GitHubIcon';
+
+interface GitHubLinkButtonProps {
+    onError: (error: unknown) => void;
+}
+
+function GitHubLinkButton({ onError }: GitHubLinkButtonProps) {
+    return (
+        <TooltipTrigger delay={0}>
+            <ActionButton
+                aria-label="GitHub"
+                onPress={() => {
+                    openExternalUrl(GITHUB_REPOSITORY_URL, onError);
+                }}
+                UNSAFE_className="fr-github-button"
+            >
+                <GitHubIcon />
+            </ActionButton>
+            <Tooltip>GitHub</Tooltip>
+        </TooltipTrigger>
+    );
+}
+
+export default GitHubLinkButton;
