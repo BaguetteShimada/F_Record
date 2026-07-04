4b46dda05c0a802da0fb3785cef8ec45cc719589
CasseShimada <c382304209@gmail.com>
2026-07-04 10:56:09 +0800

    Add injectable config store dependencies

--- generator/src/configStore.js
+++ generator/src/configStore.js
@@ -2,9 +2,15 @@ const { F_Record_Dir, configDataFilePath } = require("./paths");
 const { createDefaultConfigData, normalizeConfigData } = require("./models");
 const { ensureDirectory, readJsonFile } = require("./storage");
 
-function readConfigData(fallback) {
-    ensureDirectory(F_Record_Dir);
-    return normalizeConfigData(readJsonFile(configDataFilePath), fallback || createDefaultConfigData());
+function readConfigData(fallback, options) {
+    options = options || {};
+    const ensureDirectoryFn = options.ensureDirectory || ensureDirectory;
+    const readJsonFileFn = options.readJsonFile || readJsonFile;
+    const rootDir = options.rootDir || F_Record_Dir;
+    const filePath = options.filePath || configDataFilePath;
+
+    ensureDirectoryFn(rootDir);
+    return normalizeConfigData(readJsonFileFn(filePath), fallback || createDefaultConfigData());
 }
 
 module.exports = {
