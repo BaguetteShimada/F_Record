e74b29092472c0c29c787f9407509f24954cf02c
CasseShimada <c382304209@gmail.com>
2026-07-04 11:34:37 +0800

    Add generator test runner

---
+++ generator/test/run.js
@@ -0,0 +1,48 @@
+const path = require("path");
+const { spawnSync } = require("child_process");
+
+const testFiles = [
+    "recordingLogic.test.js",
+    "recordingEventService.test.js",
+    "pixmapSettings.test.js",
+    "pixmapRequestOptions.test.js",
+    "pixmapService.test.js",
+    "savePixmapSettings.test.js",
+    "savePixmapValidation.test.js",
+    "savePixmapBuffer.test.js",
+    "savePixmapPixel.test.js",
+    "savePixmapCopy.test.js",
+    "savePixmapImage.test.js",
+    "savePixmapWriter.test.js",
+    "savePixmapTarget.test.js",
+    "captureFramePaths.test.js",
+    "captureFrameResult.test.js",
+    "captureService.test.js",
+    "configStore.test.js",
+    "paths.test.js",
+    "models.test.js",
+    "timeSpentEligibility.test.js",
+    "timeSpentService.test.js",
+    "documentStore.test.js",
+    "storage.test.js",
+    "captureGate.test.js",
+    "mutex.test.js",
+    "pollingTask.test.js",
+    "runtimePollingTasks.test.js",
+    "runtimePollingService.test.js",
+    "documentState.test.js",
+    "documentSettingsService.test.js",
+    "documentSyncService.test.js",
+    "logger.test.js",
+    "runtime.test.js",
+];
+
+for (const testFile of testFiles) {
+    const result = spawnSync(process.execPath, [path.join(__dirname, testFile)], {
+        stdio: "inherit",
+    });
+
+    if (result.status !== 0) {
+        process.exit(result.status || 1);
+    }
+}
